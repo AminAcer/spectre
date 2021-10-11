7123b8fa13b26936d8e559ad8dd494ca620b0bd0
Vatsal <aminvatsal1999@gmail.com>
2021-10-10 22:28:54 -0500

    Added - Object tracing - Detection counter hud - No detection hud - Max objects drawn 3 - Now shows live feed with tracking

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-from spf import spectre as sp
+from spf import Spectre as sp
 
 sr1 = sp()
 
-#sr1.LiveFeed()
+# sr1.LiveFeed()
 sr1.detRed()
--- spf.py
+++ spf.py
@@ -1,22 +1,19 @@
 import cv2
 import numpy as np
+import imutils
+import heapq
 
-class spectre(object):
+
+class Spectre(object):
 
     def __init__(self):
         pass
 
-    def LiveFeed(self,):
+    def LiveFeed(self):
         winName = "SR-1 Live Feed"
         cv2.namedWindow(winName)
-        cap = cv2.VideoCapture(1)
-
-        if cap.isOpened():
-            ret, frame = cap.read()
-        else:
-            ret = False
-
-        while ret:
+        cap = cv2.VideoCapture(0)
+        while True:
             ret, frame = cap.read()
             frame = cv2.resize(frame, None, fx=1, fy=1, interpolation=cv2.INTER_AREA)
             cv2.imshow(winName, frame)
@@ -28,29 +25,51 @@ class spectre(object):
 
     def detRed(self):
         winName = "SR-1 Red Detection"
-        cv2.namedWindow(winName)
-        cap = cv2.VideoCapture(1)
+        cap = cv2.VideoCapture(0)
 
-        if cap.isOpened():
+        while True:
             ret, frame = cap.read()
-        else:
-            ret = False
+            frame = imutils.resize(frame, width=1000)
+            blur = cv2.GaussianBlur(frame, (11,11), 0)
+            hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+            redUpper = np.array([190, 255, 255])  # BRIGHTER
+            redLower = np.array([140, 150, 0])   # DARKER
 
-        while ret:
-            ret, frame = cap.read()
-            frame = cv2.resize(frame, None, fx=1, fy=1, interpolation=cv2.INTER_AREA)
+            # Making masks
+            mask = cv2.inRange(hsv, redLower, redUpper)
+            mask = cv2.erode(mask, None, iterations=2) # Removes small blobs
+            mask = cv2.dilate(mask, None, iterations=2) # Remobes small blobs
+            redMask = cv2.bitwise_and(frame, frame, mask=mask)
 
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            redUpper = np.array([190, 255, 255])  #BRIGHTER
-            redLower = np.array([140, 150, 0])   #DARKER
+            # Making contours
+            contr = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contr = imutils.grab_contours(contr)
+            center = None #Center of ball
 
-            redMask = cv2.inRange(hsv, redLower, redUpper)
-            rMask = cv2.bitwise_and(frame, frame, mask=redMask)
+            if len(contr) > 0:
+                bigc = heapq.nlargest(3, contr, key=cv2.contourArea) # Biggest contour
+                cnt = 0  # Count of detections
+                for c in bigc:
+                    ((x, y), radius) = cv2.minEnclosingCircle(c)
+                    M = cv2.moments(c)
+                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                    if radius > 5: #Only draw for contours with min radius
+                        cnt = cnt + 1
+                        cv2.circle(frame, (int(x), int(y)), int(radius), (255, 0, 20), 4)
+                        cv2.circle(frame, center, 5, (0, 255, 255), -1)
+                        cv2.putText(frame, "Detections: ", (22, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 255, 255), 1)
+                        cv2.rectangle(frame, (12, 15), (195, 60), (0, 255, 255), 2)
+                cv2.putText(frame, str(int(cnt)), (172, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 255, 255), 1)
 
-            cv2.imshow("SR-1 LiveFeed", frame)
-            cv2.imshow(winName, rMask)
+            else:
+                cv2.putText(frame, "No Detections", (22, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 2)
+                cv2.rectangle(frame, (12, 15), (215, 60), (0, 0, 255), 2)
 
             if cv2.waitKey(1) == 27:
                 break
+
+            cv2.imshow("SR-1 Live Feed", frame)
+            #cv2.imshow(winName, redMask)
+
         cap.release()
         cv2.destroyAllWindows()
